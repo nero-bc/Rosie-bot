217df646e871050d68f22af512218364acb43521
L E M O N <ryme.care@hotmail.com>
2024-02-11 21:29:24 +0530

    updated

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -6,15 +6,16 @@ from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages
 import asyncio
-        
+
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
         text='Broadcasting your messages...'
-    )
+    )   
+
+
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
@@ -23,20 +24,46 @@ async def verupikkals(bot, message):
     failed =0
 
     success = 0
+
+    sem = asyncio.Semaphore(25) # limit the number of concurrent tasks to 100
+
+    async def run_task(user):
+        async with sem:
+            res = await broadcast_func(user, b_msg)
+            return res
+
+    tasks = []
+
     async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
-        if pti:
-            success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                failed += 1
-        done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
+        task = asyncio.ensure_future(run_task(user))
+        tasks.append(task)
+        
+    for res in await asyncio.gather(*tasks):
+        success1, blocked1, deleted1, failed1, done1 = res
+        done += done1
+        blocked += blocked1
+        deleted += deleted1
+        failed += failed1
+        success += success1
+
+        if not done % 50:
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+
+
+async def broadcast_func(user, b_msg):
+        success, blocked, deleted, failed, done = 0, 0, 0, 0, 0
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if pti:
+            success = 1
+        elif pti == False:
+            if sh == "Blocked":
+                blocked=1
+            elif sh == "Deleted":
+                deleted = 1
+            elif sh == "Error":
+                failed = 1
+        done = 1
+        return success, blocked, deleted, failed, done
