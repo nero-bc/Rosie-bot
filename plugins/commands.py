99b99beea11cc1fb76eecf0132245a979e58bd4c
L E M O N <ryme.care@hotmail.com>
2024-02-12 16:42:55 +0530

    updated

--- plugins/commands.py
+++ plugins/commands.py
@@ -272,11 +272,11 @@ async def start(client, message):
         safe_decoded_date = decoded_date.decode('utf-8')  # Convert to string
         
         #verify link
-        verify = await shortlink(f"https://t.me/{temp.U_NAME}?start=Verify#{urlsafe_encoded_user_id}#{encoded_todays_date}") 
+        verifi = await shortlink(f"https://t.me/{temp.U_NAME}?start=Verify#{urlsafe_encoded_user_id}#{encoded_todays_date}") 
         is_verified = await db.fetch_value(message.from_user.id, "verified")
 
         if safe_decoded_date != todays_date:
-            return await message.reply(f"Unauthorized Access, Please use this link to verify --> {verify}")
+            return await message.reply(f"Unauthorized Access, Please use this link to verify --> {verifi}")
         elif is_verified is True:
             return await message.reply(f"<b>You are already verified</b>")
         elif decoded_user_id != message.from_user.id:
