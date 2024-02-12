8191855e7cbc99645029cde4d46b5be56597e6ec
L E M O N <ryme.care@hotmail.com>
2024-02-12 17:17:38 +0530

    updated

--- plugins/commands.py
+++ plugins/commands.py
@@ -221,7 +221,7 @@ async def start(client, message):
             user_id_bytes = str(message.from_user.id).encode('utf-8')  # Convert to bytes
             urlsafe_encoded_user_id = base64.urlsafe_b64encode(user_id_bytes).decode('utf-8')
             #verify link
-            link = f"https://t.me/{temp.U_NAME}?start=Verify#{urlsafe_encoded_user_id}#{encoded_todays_date}"
+            link = f"https://t.me/{temp.U_NAME}?start=Verify-{urlsafe_encoded_user_id}-{encoded_todays_date}"
             verifilink = await shortlink(link)
             return await message.reply(
                 f"<b>🎏 Your free limit has been reached. To continue enjoying an ad-free experience all day, please verify yourself by clicking the button below or <a href=https://t.me/{temp.U_NAME}?start=upgrade>upgrade to premium</a></b>",
@@ -253,10 +253,9 @@ async def start(client, message):
         await del_msg.edit("__⊘ This message was deleted__")
 
 
-
     # Verify system
-    elif data.split("#", 1)[0] == "Verify":
-        user_id_b64, enc_date = data.split("#", 1)[1].split("#", 1)  # Correctly split the two base64 values
+    elif data.split("-", 1)[0] == "Verify":
+        user_id_b64, enc_date = data.split("-", 1)[1].split("-", 1)  # Correctly split the two base64 values
 
         #encode user_id
         user_id_bytes = str(message.from_user.id).encode('utf-8')  # Convert to bytes
